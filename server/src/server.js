ecd1048d29f736338f21423f3698317eeff8f8f9
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-03-10 01:25:45 -0300

    Prueba con get, put y post con falsa db - Back

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,60 @@
-import express from 'express';
+import express from "express";
+
+let articleInfo = [
+  {
+    name: "learn-react",
+    upvotes: 0,
+    comments: [],
+  },
+  {
+    name: "learn-node",
+    upvotes: 0,
+    comments: [],
+  },
+];
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
+//test
+// app.get('/hello', (req, res) => {
+//     res.send('Hello World!');
+// });
 
-app.post('/hellop', (req, res) => {
-    console.log(req.body)
-    res.send(`Hello ${req.body.name}!`);
-});
+// app.post('/hellop', (req, res) => {
+//     console.log(req.body)
+//     res.send(`Hello ${req.body.name}!`);
+// });
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { username, text } = req.body;
+  const article = articleInfo.find((article) => article.name === name);
+
+  article
+    ? (article.comments.push({ username, text }),
+      res.send(`${name} now has ${article.comments.length} comments`))
+    : res.send("No article found");
+});
+
+app.put("/api/articles/:name/upvote", (req, res) => {
+  const { name } = req.params;
+  const article = articleInfo.find((article) => article.name === name);
+
+  article
+    ? ((article.upvotes += 1),
+      res.send(`${name} now has ${article.upvotes} upvotes`))
+    : res.send("No article found");
+});
+
+app.get("/api/articles/:name", (req, res) => {
+  const { name } = req.params;
+  const article = articleInfo.find((article) => article.name === name);
+
+  article ? res.send(article) : res.send("No article found");
 });
